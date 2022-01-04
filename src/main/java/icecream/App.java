9f88d32ab606f05997914e85e32aaa06ad887cd5
root <root@ip-172-31-21-112.us-east-2.compute.internal>
2022-01-04 17:54:53 +0000

    new file created

---
+++ src/main/java/icecream/App.java
@@ -0,0 +1,13 @@
+package icecream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
